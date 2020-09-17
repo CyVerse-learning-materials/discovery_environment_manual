e3d728936c6b1dbbb9a34717d946c5f30b8fb67f
JasonJWilliamsNY <JasonJWilliamsNY@gmail.com>
2020-09-17 14:45:07 -0400

    add triage issue template

--- misc/cyverse_sphinx_conf.py
+++ misc/cyverse_sphinx_conf.py
@@ -10,10 +10,10 @@ extensions = [
 ]
 
 project = 'YOUR PROJECT NAME HERE as topic_tutorial'
-copyright = '2017, CyVerse'
+copyright = '2020, CyVerse'
 author = 'CyVerse'
-version = '1.0'
-release = '1.0'
+version = '2.0'
+version = '2.0'
 
 language = None
 source_parsers = {
@@ -77,3 +77,7 @@ def setup(app):
     app.add_javascript('intercom-script-for-learning.js')
     app.add_javascript('bootstrap.min.js')
     app.add_javascript('jquery.min.js')
+    app.add_stylesheet('detail-expand.css')
+    app.add_stylesheet('question-answer.css')
+    app.add_javascript('detail-expand.js')
+    app.add_javascript('question-answer.js')
